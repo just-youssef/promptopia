b931c7a36f7cd2ffab9bb0e8a6a85fbeada9dddc
Youssef Hussein <61589133+just-youssef@users.noreply.github.com>
2023-10-08 18:49:36 +0300

    promptopia

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema({
     },
     username: {
         type: String,
-        unique: [true, 'username is already exists!'],
+        // unique: [true, 'username is already exists!'],
         // match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
     image: {
