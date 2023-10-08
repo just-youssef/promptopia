4e70e487ca847848d90244ebd42b687059fe180e
Youssef Hussein <61589133+just-youssef@users.noreply.github.com>
2023-10-08 18:26:35 +0300

    promptopia

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,11 +1,11 @@
 import Prompt from "models/prompt";
 import { connectToDB } from "utils/database";
 
-export const GET = async (request) => {
+export const GET = async (request, { params }) => {
     try {
         await connectToDB()
 
-        const prompts = await Prompt.find({ creator: '6520db950133ee3ecc756fae' }).populate('creator')
+        const prompts = await Prompt.find({}).populate('creator')
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
